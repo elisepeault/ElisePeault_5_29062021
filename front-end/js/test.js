5ac83fc08368d0880b7ad1de90e7efcae56f9623
Péault <elise.peault2@gmail.com>
2021-07-05 17:32:59 +0200

    Footer title

---
+++ front-end/js/test.js
@@ -0,0 +1,10 @@
+class Teddy {
+    constructor(id, name, price, imageUrl, description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+}
+
