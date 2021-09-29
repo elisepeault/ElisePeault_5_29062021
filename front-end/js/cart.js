0ad0ac1b99f492bf4ff0afd9a6299e1adfb358d7
Péault <elise.peault2@gmail.com>
2021-09-29 11:06:42 +0200

    comments

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -75,24 +75,29 @@ for (let product in local) {
 
 }
 
-    /* Total prices */
-  /*
-    const totalCosts = document.getElementById("total-costs");
+    /* Total costs of the teddies in the cart */
+  
+// Select the total-costs element in the DOM (where the total will be display)
+let totalCosts = document.getElementById("total-costs");
 
-  const classPrice = document.querySelectorAll(".price");
+// Select each price element in the DOM
+const classPrice = document.querySelectorAll(".price");
+
+// Array to contain prices of teddies in the cart
+let arrayOfPrices = [];
 
     function totalCart () {
       let totalCosts = 0;
       for (let product in local) {
         totalCosts += local[product].price;
-
       }
-      totalCosts.push(classPrice);
+      arrayOfPrices.push(classPrice);
+
       return totalCosts;
     }
     
     console.log( totalCart())
-*/
+
 
 
 /* "Clear the cart" Button */ 
@@ -105,6 +110,63 @@ clearTheCartButton.addEventListener("click", () => {
 
 
 /*--------------------------- SUBMIT FORM ---------------------------*/
+
+
+/* TEST */
+
+// Select the form element in the DOM
+const cartForm = document.getElementById("form");
+
+// Select the submit button of the form in the DOM
+let orderButton = document.getElementById("order-button");
+
+// Select each input of the form in the DOM
+let firstNameInput = document.getElementById("firstName");
+let lastNameInput = document.getElementById("lastName");
+let addressInput = document.getElementById("address");
+let cityInput = document.getElementById("city");
+let emailInput = document.getElementById("email");
+
+// the array "purchasedProducts" will contain objects = the teddies that were purchased by the customer
+let purchasedProducts = [];
+//purchasedProducts.push(local);
+
+// the const "order" contains the object contact (information about the customer) and the array containing the purchased products
+const order = {
+  contact: {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    address: addressInput.value,
+    city: cityInput.value,
+    email: emailInput.value,
+  },
+  products: purchasedProducts,
+};
+
+// add the "on submit" event listener to the form
+cartForm.addEventListener("submit", function (e) {
+  //orderButton.addEventListener("submit", function (e) {
+  //prevent the page from reloading or naviguating away when we submit the form
+  e.preventDefault();
+
+  fetch("http://localhost:3000/api/teddies/order", {method: "POST", body: JSON.stringify(order)})
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    alert("Il y a eu une erreur : " + error);
+  });
+
+});
+
+
+
+
+
+
+/* !!!  PREMIERE TECHNIQUE  !!! */
+
 /*
 // On fait un lien vers le formulaire dans le DOM (Get a reference to the Form element in the DOM )    myForm => id of the form !!!
 const myForm = document.getElementById("");
@@ -115,8 +177,82 @@ myForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   // sending the data using fetch  (2 techniques!!!)
-  const formData
 
+  //First technique  : we pass trough the formData using a FormData object
+  const formData = new FormData(this); // this => refers to the actual form itself.  The object "new FormData" contains a pair of keys and values which match the elements in the form (ex : city, with the value that will be chosen by the user)
+
+  fetch("http://localhost:3000/api/teddies/order", {method: "post", body: formData})
+    .then(function (response) {
+      return response.text();
+    })
+    .then (function (text) {
+      console.log(text);
+    })
+    .catch (function (error) {
+      console.error (error);
+    })
 
 });
+*/
+
+/* !!!  DEUXIEME TECHNIQUE  !!! */
+
+/*
+  //we pass through an instance of the url search params ????
+  
+  const myForm = document.getElementById("");
+
+// add the "on submit" event listener to the form
+myForm.addEventListener("submit", function (e) {
+  //prevent the page from reloading or naviguating away when we submit the form
+  e.preventDefault();
+  
+  const formData = new FormData(this); 
+  const searchParams = new URLSearchParams(); // send the data through an url enconding strings
+
+  for (const pair of formData) {
+    searchParams.append(pair[0], pair[1]) // pair 0 = ex city and pair 1 = value
+  }
+
+  fetch("http://localhost:3000/api/teddies/order", {method: "post", body: searchParams})
+  .then(function (response) {
+    return response.text();
+  })
+  .then (function (text) {
+    console.log(text);
+  })
+  .catch (function (error) {
+    console.error (error);
+  })
+
+});
+*/
+
+
+
+/* !!!  TROISIEME TECHNIQUE  !!! */
+
+/*
+fetch("http://localhost:3000/api/teddies/order", {method: "post", body: searchParams})
+.then(results => results.json())
+.then(console.log);
+.catch((error) => {
+  alert("Il y a eu une erreur : " + error);
+});
+
+*/
+
+
+/* !!!  DONNEES DU BACK END  !!! */
+
+//Expects request to contain:
+/*
+contact: {
+  firstName: string,
+  lastName: string,
+  address: string,
+  city: string,
+  email: string
+}
+products: [string] <-- array of product _id
 */
