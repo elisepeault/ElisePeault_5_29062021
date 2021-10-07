e1963932e070e213f728ecf36184fa3fc513fd85
Péault <elise.peault2@gmail.com>
2021-10-07 15:45:29 +0200

    Delete Regex

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -92,65 +92,42 @@ let orderButton = document.getElementById("order-button");
 /* --- Add the "on submit" EVENT LISTENER to the form --- */ 
 cartForm.addEventListener("submit", function (e) {
   
-  // REGEX for the email field validation 
-  let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  // REGEX to check if a field is empty 
-  let emptyFieldRegex = /^[\s]/;
-
-  //Check if the fields of the form are valid 
-  // if the email adress doesn't match the expected syntax : alert message
-  if (
-    emailRegex.test(emailInput.value) == false 
-    ) {
-    window.alert("La syntaxe de l'adresse email n'est pas valide.");
-    e.preventDefault(); // prevent the page from reloading or naviguating away
-    //if one or many fields are empty : alert message
-  } else if (
-    emptyFieldRegex.test(firstNameInput.value) == true ||
-    emptyFieldRegex.test(lastNameInput.value) == true ||
-    emptyFieldRegex.test(addressInput.value) == true ||
-    emptyFieldRegex.test(cityInput.value) == true ||
-    emptyFieldRegex.test(emailInput.value) == true 
-  ) {
-    window.alert("Merci de remplir tous les champs du formulaire.");
-    // else => If fields are valid : purchased products & delivery info will be sent with the post request (details below)
-  } else {
+  // prevent the page from reloading or naviguating away
+  e.preventDefault();
 
     // the array "purchasedProducts" will contain objects = the teddies that were purchased by the customer
-      let purchasedProducts = [];
+    let purchasedProducts = [];
 
-      for (let product in local) {
-        purchasedProducts.push(local[product].id);
-      }
+    for (let product in local) {
+      purchasedProducts.push(local[product].id);
+    }
 
-      // the const "order" contains the object contact (info for the delivery) and the array products (purchased products)
-      const order = {
-        contact: {
-          firstName: firstNameInput.value,
-          lastName: lastNameInput.value,
-          address: addressInput.value,
-          city: cityInput.value,
-          email: emailInput.value,
-        },
-        products: purchasedProducts,
-      };
-      //console.log(order);
+    // the const "order" contains the object contact (info for the delivery) and the array products (purchased products)
+    const order = {
+      contact: {
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+        address: addressInput.value,
+        city: cityInput.value,
+        email: emailInput.value,
+      },
+      products: purchasedProducts,
+    };
+    //console.log(order);
 
-      fetch("http://localhost:3000/api/teddies/order", {method: "POST", body: JSON.stringify(order), headers: {"Content-Type":"application/json"} })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data); 
-        localStorage.clear();
-        localStorage.setItem("orderId", data.orderId);
-        // Redirect to the order-confirmation page (once the order is validated and the order id is saved in the local storage)
-        document.location.href = "order-confirmation.html";
-        //console.log(data.orderId); //numéro de commande à récupérer du back
-      })
-      .catch((error) => {
-        alert("Il y a eu une erreur : " + error);
-      });
-
-  }
+    fetch("http://localhost:3000/api/teddies/order", {method: "POST", body: JSON.stringify(order), headers: {"Content-Type":"application/json"} })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data); 
+      localStorage.clear();
+      localStorage.setItem("orderId", data.orderId);
+      // Redirect to the order-confirmation page (once the order is validated and the order id is saved in the local storage)
+      document.location.href = "order-confirmation.html";
+      //console.log(data.orderId); //numéro de commande à récupérer du back
+    })
+    .catch((error) => {
+      alert("Il y a eu une erreur : " + error);
+    });
 
 });
 
